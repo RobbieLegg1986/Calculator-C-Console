44e97429d8864317bc135598f7dcf20a5e3a68bf
RobbieLegg1986 <72741406+RobbieLegg1986@users.noreply.github.com>
2020-10-12 12:42:29 +0100

    Add files via upload

---
+++ better calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace building_a_better_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if ( op == "+")
+            {
+                Console.Write(num1 + num2);
+            } 
+            else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            }
+
+            else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+            }
+
+     else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
